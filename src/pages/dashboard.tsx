a726148bef173147a08b249eee26816161cf4c7c
Rynex Native <rynexnative@gmail.com>
2025-12-31 18:00:05 +0300

    DASHBOARD DESIGN: nimeendelea kufanya maboresho ya muonekano wa dashboard kwenye color na mpangilio wa maudhui ya ndani yake...ili kuhakikisha kila kitu kipo sawa na kuhakikisha muonekano bora wa content na rahisi kwa matumizi ya USERs

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -10,16 +10,16 @@ function dashboard() {
       paddingLeft: "2rem",
       paddingRight: '1rem'
     }}>
-      <div className='col-span-4 h-6 '>sdkjahdjkas</div>
-        <Card variant="primary" className="flex rounded-2xl w-full col-span-3 mt-3 h-[10rem] relative bg-gradient-to-r 
+      <div className='col-span-4 h-6 '>Home</div>
+        <Card variant="primary" className="flex rounded-2xl w-full col-span-3 mt-3 h-[14rem] relative bg-gradient-to-r 
             from-[#0180E196] from-[42%] 
-            to-[#FFFFFF96] to-[100%] sm:col-span-5">
-          <div className='w-[80%] mr-1 md:w-[70%]'>
-          <CardHeader className="border-b-0 m-0 py-0 sm:py-0 text-[1rem] md:text-[0.8rem] lg:text-[1.6rem] text-[#003B6C] font-[1000]">"Knowledge is not power until applied."</CardHeader>
-          <CardContent className='m-0 px-1 py-0 text-[0.6em] sm:py-0 text-[0.5em]'>Knowledge alone doesn’t create change — it’s only when you apply what you know that it becomes true power. Learning gives you potential, but action unlocks that potential. Without application, knowledge remains just information, not transformation.
+            to-[#FFFFFF96] to-[100%]  px-0 md:col-span-5 lg:col-span-3 col-span-5">
+          <div className='w-[80%] mr-1 md:w-[70%] py-0'>
+          <CardHeader className="border-b-0 m-0 py-0 md:text-[0.8rem] lg:text-[1.5rem] lg:py-1 sm:text-[1rem] text-[#003B6C] font-[1000]">"Knowledge is not power until applied."</CardHeader>
+          <CardContent className='m-0 px-1 py-0 text-[0.6em] sm:py-0 text-[0.5em] lg:text-[1rem]'>Knowledge alone doesn’t create change — it’s only when you apply what you know that it becomes true power. Learning gives you potential, but action unlocks that potential. Without application, knowledge remains just information, not transformation.
 It’s a reminder that real growth and success come from doing, not just knowing. </CardContent>
-          <CardFooter className='border-t-0 py-0 sm:py-0'>
-            hdsajdjasd
+          <CardFooter className='border-t-0 lg:py-2 sm:py-0 lg:text-[0.8rem] lg:text-[0.7rem] md:text-[0.7rem] font-[600] text-[black]'>
+            ON 12 AUG 2025
           </CardFooter>
           </div>
           <img src={Image1} alt="" style={{
@@ -30,23 +30,38 @@ It’s a reminder that real growth and success come from doing, not just knowing
           }} className='sm-w-[12px]'/>
           
         </Card>
-        <Card variant="primary" className='col-span-2 mt-3 bg-gradient-to-r from-[#FF383C80] to-[#FFFFFF] '>
-          <CardHeader>Gas Inventory</CardHeader>
-          <CardContent>100 KG Available</CardContent>
+        <Card variant="primary" className='col-span-2 text-[1.2rem] mt-3 bg-gradient-to-r from-[#CA590394] from-[10%] to-[#FFFFFF] to-[120%] '>
+          <CardHeader>Welcome Mr Deo Massawe</CardHeader>
+          <CardContent className='flex gap-4 items-start text-[1.2rem]'>
+            <img src={Image1} alt="" className="w-[7rem] h-[7rem] rounded-full"/>
+            <div>
+              Today: 25th Dec 2025 <br />
+              Role: Head Master <br />
+              Subject: Mathematics <br />
+              Age: 20 Years
+            </div>
+          </CardContent>
         </Card>
         <Card variant="primary" className="lg:col-span-1 sm:col-span-2 relative">
-          <CardHeader>Gas Inventory</CardHeader>
+          <CardHeader className='border-b-0 items-center'>Upcoming Lesson</CardHeader>
           <CardContent>100 KG Available</CardContent>
           
         </Card>
 
         <Card variant="primary" className="lg:col-span-1 sm:col-span-2 relative">
-          <CardHeader>Gas Inventory</CardHeader>
+          <CardHeader className='border-b-0'>Gas Inventory</CardHeader>
           <CardContent>100 KG Available</CardContent>
           
         </Card>
 
         <Card variant="primary" className="lg:col-span-1 sm:col-span-2 relative bg-gradient-to-r 
+            from-[red]  
+            to-[blue]">
+          <CardHeader>Gas Inventory</CardHeader>
+          <CardContent>100 KG Available</CardContent>
+          
+        </Card>
+        <Card variant="primary" className="lg:col-span-2 row-span-4 h-[20rem] sm:col-span-2 relative bg-gradient-to-r 
             from-[red]  
             to-[blue]">
           <CardHeader>Gas Inventory</CardHeader>
